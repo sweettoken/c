1b72aee622c7bfe99bbf79acf918d2b927f4e3e9
xiaocao <nw_1116@126.com>
2022-04-25 23:36:56 +0800

    阶段性提交

---
+++ datatypedemo/4.浮点型和字符型/main.c
@@ -0,0 +1,10 @@
+//#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	float f = 1.234;
+	printf("输出浮点数 %f\n", f);
+	char c = 'a';
+	printf("字符型变量 %c\n", c);
+	printf("字符型变量 %d", c);
+} 
---
+++ datatypedemo/5.字符型大小写转换/main.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	char a = 'a';
+	a = a - 32;
+	printf("%c", a);
+}
